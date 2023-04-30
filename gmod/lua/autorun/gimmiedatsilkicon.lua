ddd3265de3d837d386df2461b140dafbd84750b7
donaldtrumpforthewin <jaydenmoritz@gmail.com>
2023-04-29 22:14:33 -0400

    Added floodgate

--- gmod/lua/autorun/gimmiedatsilkicon.lua
+++
@@ -1,7 +0,0 @@
---the clients need the ttt icon i used in the menu so we will send it here via the workshop
-if SERVER then
-	resource.AddFile("materials/icon16/addons.png")
-	resource.AddFile("materials/icon16/ttt.png")
-
-	resource.AddWorkshop("314701418")
-end
